86a17d774c37ff9312739d546e1ac81f194dcd11
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-09-12 20:43:15 -0400

    fixed up id name

--- js/main.js
+++ js/main.js
@@ -11,8 +11,8 @@ Cookies.set('userName', siteUserName);
 console.log('Setting cookie with name value: ' + Cookies.get('userName'));
 
 document.getElementById('submitButton').onclick = function(event) { 
-	console.log('the button works for now')
-	Cookies.set('userName', document.getElementById('submitButton').value)
+	console.log('the button works for now');
+	Cookies.set('userName', document.getElementById('fname').value);
 	console.log(Cookies.get('userName'));
 };
 //var siteUserName = Cookies.get('userName');
