7dae1b7f0dbbb16a2881032f940c02d4ea89401d
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-09-12 20:36:03 -0400

    button works, but does the variable?

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ console.log('The site user name is ' + siteUserName + '.');
 Cookies.set('userName', siteUserName);
 console.log(Cookies.get('userName'));
 
-
+document.getElementById('submitButton').onclick = function(event) { 
+	console.log('the button works for now')
+	console.log(Cookies.get('userName'));
+};
 //var siteUserName = Cookies.get('userName');
 //console.log(siteUserName);
