0662a0d19e98655ae1d3bd7825dc766a413af282
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-09-12 20:41:23 -0400

    testing more

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ console.log('Setting cookie with name value: ' + Cookies.get('userName'));
 
 document.getElementById('submitButton').onclick = function(event) { 
 	console.log('the button works for now')
+	Cookies.set('userName', document.getElementById('submitButton').value)
 	console.log(Cookies.get('userName'));
 };
 //var siteUserName = Cookies.get('userName');
