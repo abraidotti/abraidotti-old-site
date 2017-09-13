eab624e36efcc68f004a5651c1d29c752c418ec4
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-09-12 20:29:37 -0400

    spitting out site user name via cookie

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 console.log('main.js works.');
 console.log('cookies.js hooked up');
 
-Cookies.set('userName', 'Bud');
+// get the value of an input
+// save it in a cookie
+
+var siteUserName = document.getElementById("fname").value;
+console.log('The site user name is ' + siteUserName + '.');
+
+Cookies.set('userName', siteUserName);
+console.log(Cookies.get('userName'));
 
 
-var siteUserName = Cookies.get('userName');
-console.log(siteUserName);
+//var siteUserName = Cookies.get('userName');
+//console.log(siteUserName);
